f73932b9b980b988b7df612cc54a58eef8133f3c
MorhunBohdan <eco.mbogdan@gmail.com>
2022-08-25 19:01:02 +0300

    Added gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 let gulp = require('gulp'),
+    autoprefixer = require("gulp-autoprefixer"),
     sass = require('gulp-sass')(require('sass'));
 
 
 function buildStyles() {
     return gulp.src('assets/scss/**/*.scss', "!" + "assets/scss/**/_*.scss")
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+                overrideBrowserList: ["last 5 versions"],
+                cascade: true
+            })
+        )
         .pipe(gulp.dest('src/css'));
 }
 
